9454cfb9545f06d1e83ef62f1fa70a515bb26d4a
Andy Peterson <andy.peterson@atomicobject.com>
2016-09-27 17:35:17 -0400

    Initial Commit

---
+++ chat.elm
@@ -0,0 +1,74 @@
+import Html exposing (..)
+import Html.App as App
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import WebSocket
+
+main : Program Never
+main =
+  App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+-- MODEL
+
+type alias Model =
+  { input : String
+  , messages : List String
+  }
+
+
+init : (Model, Cmd Msg)
+init =
+  (Model "" [], Cmd.none)
+
+
+-- UPDATE
+
+type Msg
+  = Input String
+  | Send
+  | NewMessage String
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Input newInput ->
+      ({ model | input = newInput }, Cmd.none)
+
+    Send ->
+      ({ model | input = "" }, WebSocket.send "ws://echo.websocket.org" model.input)
+      -- ( { model | input = "" }
+      -- , Horizon.insertCmd collectionName (newMessageEncoder model.input |> toList)
+      -- )
+
+    NewMessage str ->
+      ({ model | messages = (List.append model.messages [str]) }, Cmd.none)
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  WebSocket.listen "ws://echo.websocket.org" NewMessage
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ div [] (List.map viewMessage model.messages)
+    , input [onInput Input, value model.input] []
+    , button [onClick Send] [text "Send"]
+    ]
+
+
+viewMessage : String -> Html msg
+viewMessage msg =
+  div [] [ text msg ]
